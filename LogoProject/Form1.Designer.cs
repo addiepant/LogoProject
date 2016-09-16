6d75980a2350e2d4a6869e58360d9d685985bf56
temp <temp>
2016-09-16 13:20:34 -0400

    Add project files.

---
+++ LogoProject/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace LogoProject
+{
+    partial class mainScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.countdownTimer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.FloralWhite;
+            this.startButton.Location = new System.Drawing.Point(57, 163);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(175, 86);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Play Game";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // countdownTimer
+            // 
+            this.countdownTimer.AutoSize = true;
+            this.countdownTimer.Font = new System.Drawing.Font("Liberation Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countdownTimer.Location = new System.Drawing.Point(53, 48);
+            this.countdownTimer.Name = "countdownTimer";
+            this.countdownTimer.Size = new System.Drawing.Size(0, 24);
+            this.countdownTimer.TabIndex = 1;
+            // 
+            // mainScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.NavajoWhite;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.countdownTimer);
+            this.Controls.Add(this.startButton);
+            this.Name = "mainScreen";
+            this.Text = "SCSS Loggo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label countdownTimer;
+    }
+}
+
---
+++ LogoProject/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace LogoProject
+{
+    public partial class mainScreen : Form
+    {
+        public mainScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.DarkSlateBlue; //change bacground to Dark Slate Blue
+            countdownTimer.Text = "Starting in ...3"; // Starting in...3 appears (countdown)
+
+            Refresh();
+            Thread.Sleep(1000); // Pauses for 1000 ms
+
+            countdownTimer.Text = "Starting in ..2"; // Starting in ...3 changes to Starting in ...2
+
+            Refresh();
+            Thread.Sleep(1000); // Pauses for 1000 ms
+
+            countdownTimer.Text = "Staring in ...1"; // Starting in ... 2 changes to Starting in 1
+
+            Refresh();
+            Thread.Sleep(1000); // Pauses for 1000 ms
+
+            countdownTimer.Text = "GO!"; // Starting in...1 changes to GO!
+
+            this.BackColor = Color.Salmon; // Background colour changes to Salmon
+
+            Refresh();
+            Thread.Sleep(1000); // Pauses for 1000ms
+
+            countdownTimer.Text = "";
+            this.BackColor = Color.NavajoWhite; // Background colour changes to NavajoWhite
+
+            Graphics formGraphics = this.CreateGraphics(); // set-up to have formGrahics set up as a graphic
+
+            Pen drawPen = new Pen(Color.Red, 10);
+           
+           formGraphics.DrawArc(drawPen, 20, 20, 50, 50, 30, 300);
+
+            Refresh();
+            Thread.Sleep(2000);
+
+
+        }
+    }
+}
